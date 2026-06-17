4053480250d2a27fcefffc3b4f7a19dcbd2101b4
vishnuprasad ranganatha <vishnuprasadranganatha@gmail.com>
2026-06-17 11:13:58 +0530

    supabase connection implemented

---
+++ backend/src/config/db.ts
@@ -0,0 +1,41 @@
+import postgres from "postgres";
+process.loadEnvFile(".env");
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined");
+}
+const sql = postgres(connectionString, {
+  onnotice: () => {},
+});
+
+export default sql;
+
+export const initializeDatabase = async () => {
+  await sql`
+    CREATE TABLE IF NOT EXISTS users (
+      id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      email VARCHAR(255) NOT NULL UNIQUE,
+      username VARCHAR(50) UNIQUE,
+      password VARCHAR(255) NOT NULL,
+      name VARCHAR(100) NULL,
+      description TEXT,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+  `;
+  await sql`
+  CREATE TABLE IF NOT EXISTS tasks (
+      id BIGSERIAL PRIMARY KEY,
+        user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+        title TEXT NOT NULL,
+        description TEXT,
+        priority TEXT CHECK (priority IN ('low', 'medium', 'high')),
+        status TEXT DEFAULT 'pending'
+            CHECK (status IN ('pending', 'in_progress', 'completed')),
+        due_date TIMESTAMPTZ,
+        created_at TIMESTAMPTZ DEFAULT NOW(),
+        updated_at TIMESTAMPTZ DEFAULT NOW()
+    );
+  `;
+  console.log("database initialized");
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,14 @@
 // The entry point of the application.
 import app from "./app";
+import { initializeDatabase } from "./config/db";
 process.loadEnvFile(".env");
 
 const port = process.env.PORT || 3000;
+async function start() {
+  await initializeDatabase();
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+}
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+start();
