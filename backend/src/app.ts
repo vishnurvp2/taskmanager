a444049db56852515d95644ceaea988418dd2149
vishnuprasad ranganatha <vishnuprasadranganatha@gmail.com>
2026-06-18 13:36:11 +0530

    development branch has fulfilled major features

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,12 +8,27 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   }),
+// );
+
+const allowedOrigins = ["http://localhost:5173", "http://localhost:4173"];
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(Logger);
